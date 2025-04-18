824707de47fc03f39e626e18503d5cbce10cc7d4
Joyce  Mbugua <123602165+Joymez@users.noreply.github.com>
2025-04-18 11:24:35 +0300

    Completed answers.sql

--- answers.sql
+++ answers.sql
@@ -1,5 +1,6 @@
+
+
 -- Question 1: Achieving 1NF (First Normal Form)
--- Splitting the products into individual rows so each order has one product
 
 CREATE TABLE ProductDetail_1NF (
   OrderID INT,
@@ -17,7 +18,6 @@ VALUES
 (103, 'Emily Clark', 'Phone');
 
 -- Question 2: Achieving 2NF (Second Normal Form)
--- Removing partial dependencies by splitting the table into Orders and OrderItems
 
 CREATE TABLE Orders (
   OrderID INT PRIMARY KEY,
@@ -32,10 +32,8 @@ CREATE TABLE OrderItems (
   FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- Inserting unique orders into the Orders table
 INSERT INTO Orders (OrderID, CustomerName)
 SELECT DISTINCT OrderID, CustomerName FROM OrderDetails;
 
--- Inserting order items into the OrderItems table
 INSERT INTO OrderItems (OrderID, Product, Quantity)
 SELECT OrderID, Product, Quantity FROM OrderDetails;
